81b6c526184c69f1592fd7e69d92db7db7641ae2
amankas864 <amankas864@gmail.com>
2022-07-23 16:56:31 +0530

    commanline arguments

--- 1-First Program/First.java
+++ 1-First Program/First.java
@@ -1,5 +1,6 @@
 public class First {
     public static void main(String[] args) {
-        System.out.println("first program");
+        System.out.println(args[0]);
+        System.out.println("First Program");
     }
 }
