69eb448d420a8860fbf2d4351e0ac5be99175ed1
amankas864 <amankas864@gmail.com>
2022-07-24 17:40:20 +0530

    loops initiated

--- 2-Conditions and loops/Conditions/src/Loops.java
+++ 2-Conditions and loops/Conditions/src/Loops.java
@@ -5,10 +5,21 @@ public class Loops {
         //for loop
         Scanner in = new Scanner(System.in);
         int n=in.nextInt();
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.println(i);
         }
 
         //while loop
+        int i=1;
+        while (i<=n){
+            System.out.println(i);i++;
+        }
+
+        //do while
+        i=0;
+        do {
+            System.out.print("newline req.");
+            i++;
+        }while(i<n);
     }
 }
