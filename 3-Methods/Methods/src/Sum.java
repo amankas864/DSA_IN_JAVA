465e4518a8331a2985ec597f08b3f6179bf1b059
amankas864 <amankas864@gmail.com>
2022-07-25 19:38:30 +0530

    function arguments

--- 3-Methods/Methods/src/Sum.java
+++ 3-Methods/Methods/src/Sum.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args) {
         System.out.println(sum());
+        add(5,6);
     }
+
+    private static void add(int a, int b) {
+        System.out.println(a+b);
+    }
+
     static int sum(){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the first and second no. : ");
