3e495a8ee72e306ad3de983d5d5cf38aecf21486
amankas864 <amankas864@gmail.com>
2022-07-25 21:28:54 +0530

    VarArgs and function overloading

--- 3-Methods/Methods/src/VarArgs.java
+++ 3-Methods/Methods/src/VarArgs.java
@@ -3,6 +3,11 @@ import java.util.Arrays;
 public class VarArgs {
     public static void main(String[] args) {
         foo(4,4,"name","variables");
+        foo(6,89,56);
+    }
+
+    private static void foo(int i, int i1, int i2) {
+        System.out.println("i = " + i + ", i1 = " + i1 + ", i2 = " + i2);
     }
 
     private static void foo(int a,int b, String ...c) {
