ffbfa7cd9f75b163851c2c409894ea739965ea6d
amankas864 <amankas864@gmail.com>
2022-07-26 22:34:08 +0530

    Linear search for Strings created

---
+++ 5-LinearSearch/Linear/src/StrSearch.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StrSearch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String nums= "{5,3,7,9}";
+        char target=in.next().trim().charAt(0);
+        boolean res = linearsearch(nums,target);
+        System.out.println("nums = " + nums + res);
+
+    }
+
+    private static boolean linearsearch(String a, char b) {
+        for(int j=0;j<a.length();j++){
+            if (a.charAt(j)==b)
+                return true;
+        }
+        return false;
+    }
+}
