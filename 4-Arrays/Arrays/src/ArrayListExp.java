5826c213cfc5ed858f40c6901a3a9d76dbed71d8
amankas864 <amankas864@gmail.com>
2022-07-26 19:19:40 +0530

    Multidimensional Arraylisting

--- 4-Arrays/Arrays/src/ArrayListExp.java
+++ 4-Arrays/Arrays/src/ArrayListExp.java
@@ -1,12 +1,23 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExp {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         ArrayList<Integer> lst = new ArrayList<>(10);
         lst.add(56);
         lst.add(5);
         lst.add(6);
         lst.add(65);
         System.out.println("lst = " + lst);
+        lst.set(2,656);
+        lst.remove(3);
+        for (int i = 0; i <5; i++) {
+            lst.add(in.nextInt());
+        }
+        System.out.println();
+        for (Integer integer : lst) {
+            System.out.print(integer + " ");
+        }
     }
 }
