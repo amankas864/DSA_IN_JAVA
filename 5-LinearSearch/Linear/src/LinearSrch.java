dccff354af71cecd16b8a1728814e661e99d2b43
amankas864 <amankas864@gmail.com>
2022-07-28 22:22:57 +0530

    Adding index limits to the search

--- 5-LinearSearch/Linear/src/LinearSrch.java
+++ 5-LinearSearch/Linear/src/LinearSrch.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 public class LinearSrch {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] nums={5,3,7,9};
+        int[] nums={5,3,7,9,89,76,18,59,37};
         int target=in.nextInt();
-        boolean res = linearsearch(nums,target);
+        String res = linearsearch(nums,target,1,6);
         System.out.println("nums = " + Arrays.toString(nums) + res);
         
     }
 
-    private static boolean linearsearch(int[] a, int b) {
-        for(int j : a){
-            if (j==b)
-                return true;
+    private static String linearsearch(int[] a, int b,int s,int e) {
+        for (int i = s; i <= e; i++) {
+            int j = a[i];
+            if (j == b)
+                return "\nTrue at "+i;
         }
-        return false;
+        return "None";
     }
 }
