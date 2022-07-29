9746034b2351700ab9fa70e87a5b112221a65da9
amankas864 <amankas864@gmail.com>
2022-07-29 19:32:59 +0530

    Order agnostic Binary Search

--- 6-BinarySearch/Binary/src/BinaryS.java
+++ 6-BinarySearch/Binary/src/BinaryS.java
@@ -1,30 +1,40 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+//Order agnostic
+
 public class BinaryS {
     public static void main(String[] args) {
-        int[] arr1 = {1,2,3,4,5,6,7},arr2 = {1,2,3,4,5,6,7,8};
+        int[] arr1 = {1,2,3,4,5,6,7},arr2 = {7,6,5,4,3,2,1};
         Scanner in = new Scanner(System.in);
         System.out.print("Enterthetarget:");
         int target = in.nextInt();
         System.out.println(Arrays.toString(arr1));
         //int index=BinarySrch(arr2,5);
-        System.out.println(Arrays.binarySearch(arr1,target));
+//        System.out.println(Arrays.binarySearch(arr1,target));
         System.out.println(BinarySrch(arr1,target));
-        System.out.println(Arrays.binarySearch(arr2,target));
+//        System.out.println(Arrays.binarySearch(arr2,target));
         System.out.println(BinarySrch(arr2,target));
     }
 
     private static int BinarySrch(int[] a,int b) {
         int start=0,end=a.length-1,mid;
-        while (start<=end){
-            mid = start+ (end-start)/2;
-            if (a[mid]==b)
+        boolean ascend=a[a.length-1]>a[0];
+        while (start<=end) {
+            mid = start + (end - start) / 2;
+            if (a[mid] == b)
                 return mid;
-            else if (b>a[mid]) {
-                start=mid+1;
-            } else if (b<a[mid]) {
-                end=mid-1;
+            if (ascend){
+            if (b > a[mid])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }else {
+                if (b<a[mid]) {
+                    start=mid+1;
+                } else  {
+                    end=mid-1;
+                }
             }
         }
         return -1;
