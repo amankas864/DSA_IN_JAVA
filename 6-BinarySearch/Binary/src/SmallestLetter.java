9909db5adf37797750c95ea102046957a50db91a
amankas864 <amankas864@gmail.com>
2022-07-30 14:29:32 +0530

    Smallest Letter updated

--- 6-BinarySearch/Binary/src/SmallestLetter.java
+++ 6-BinarySearch/Binary/src/SmallestLetter.java
@@ -1,8 +1,10 @@
 class SmallestLetter{
     public static void main(String[] args) {
-        System.out.println(2%2);
+        char[] a={'a','b','v'};
+        char b='j';
+        System.out.println(BinarySrch(a,b));
     }
-    private static int BinarySrch(int[] a,int b) {
+    private static int BinarySrch(char[] a,char b) {
         int start=0,end=a.length-1,mid;
         while (start<=end) {
             mid = start + (end - start) / 2;
