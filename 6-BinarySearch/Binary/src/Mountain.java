35e72338393dfca95affbfaef687ac622e5c90a8
amankas864 <amankas864@gmail.com>
2022-07-31 20:36:23 +0530

    Searching in a Mountain array

--- 6-BinarySearch/Binary/src/Mountain.java
+++ 6-BinarySearch/Binary/src/Mountain.java
@@ -11,4 +11,46 @@ public class Mountain {
             else start=mid+1;}
         return start;
     }
+
+    public int findInMountainArray(int target, int[] arr) {
+        int peak =peakIndexInMountainArray(arr);
+        int a=BinarySearchp(arr,target,peak);
+        if (a!=-1)
+            return a;
+        else {
+            a=BinarySearch(arr,target,peak);
+            return a;
+        }
+
+    }
+    private static int BinarySearchp(int[] a,int b,int p) {
+        int start=0,end=p,mid;
+        while (start<=end) {
+            mid = start + (end - start) / 2;
+            if (a[mid] == b)
+                return mid;
+
+            if (b > a[mid])
+                start = mid + 1;
+            else
+                end = mid - 1;
+
+        }
+        return -1;
+    }
+    private static int BinarySearch(int[] a,int b,int p) {
+        int start=p+1,end=a.length-1,mid;
+        while (start<=end) {
+            mid = start + (end - start) / 2;
+            if (a[mid] == b)
+                return mid;
+
+            if (b < a[mid])
+                start = mid + 1;
+            else
+                end = mid - 1;
+
+        }
+        return -1;
+    }
 }
