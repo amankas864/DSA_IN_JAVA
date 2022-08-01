6d07f3d560bc760df88fa5d2c98e6920b2c478a3
amankas864 <amankas864@gmail.com>
2022-08-01 22:47:29 +0530

    Rotated Array with duplicates

--- 6-BinarySearch/Binary/src/Rotated.java
+++ 6-BinarySearch/Binary/src/Rotated.java
@@ -45,4 +45,38 @@ public class Rotated {
         }
         return -1;
     }
+    static int findPivotWithDuplicates(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
 }
