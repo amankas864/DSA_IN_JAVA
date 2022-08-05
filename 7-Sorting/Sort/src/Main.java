4d46e843b442d845235a58323558a67f51ad0fa1
amankas864 <amankas864@gmail.com>
2022-08-05 22:04:48 +0530

    Bubble sort

--- 7-Sorting/Sort/src/Main.java
+++ 7-Sorting/Sort/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(BubbleSort());
     }
+
+    private static int BubbleSort() {
+        return 0;
+    }
+
 }
