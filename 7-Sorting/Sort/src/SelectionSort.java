78413e19c2ad3e33625f9205c963d628d12b0cfb
amankas864 <amankas864@gmail.com>
2022-08-07 14:16:15 +0530

    Selection Sort

--- 7-Sorting/Sort/src/SelectionSort.java
+++ 7-Sorting/Sort/src/SelectionSort.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
     int[] arr={3,2,1};
     selectionSort(arr);
+        System.out.println("Array = " + Arrays.toString(arr));
     }
 
     private static void selectionSort(int[] arr) {
@@ -11,9 +14,11 @@ public class SelectionSort {
                 if (arr[j]>arr[max])
                     max=j;
             }
-            arr[max]=arr[max]+arr[arr.length-i-1];
-            arr[arr.length-i-1]=arr[max]-arr[arr.length-i-1];
-            arr[max]-=arr[arr.length-i-1];
+            if (max==arr.length-i-1)
+                continue;
+            int temp =arr[max];
+            arr[max]=arr[arr.length-i-1];
+            arr[arr.length-i-1]=temp;
 
 
         }
