3ec911eb76e4414afc401e6470910b70e4f1c1b4
amankas864 <amankas864@gmail.com>
2022-08-07 14:06:35 +0530

    Sorting using selection

--- 7-Sorting/Sort/src/SelectionSort.java
+++ 7-Sorting/Sort/src/SelectionSort.java
@@ -5,6 +5,18 @@ public class SelectionSort {
     }
 
     private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int max=0;
+            for (int j = 0; j < arr.length-i; j++) {
+                if (arr[j]>arr[max])
+                    max=j;
+            }
+            arr[max]=arr[max]+arr[arr.length-i-1];
+            arr[arr.length-i-1]=arr[max]-arr[arr.length-i-1];
+            arr[max]-=arr[arr.length-i-1];
+
+
+        }
 
 
     }
