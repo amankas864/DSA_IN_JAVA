57d1b067a0950a5ce44e632763d1b583c22e9c8a
amankas864 <amankas864@gmail.com>
2022-08-08 22:32:37 +0530

    partial sorting

---
+++ 7-Sorting/Sort/src/InsertionSort.java
@@ -0,0 +1,10 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr= new int[]{5,4,3,2,1};
+        insertion(arr);
+    }
+
+    private static void insertion(int[] arr) {
+    //partial sorting
+    }
+}
