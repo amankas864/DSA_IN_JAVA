f37c20bca2db0b47b7bc68819b6dea1c94aefde5
amankas864 <amankas864@gmail.com>
2022-08-23 21:51:17 +0530

    Insertion sort

--- 7-Sorting/Sort/src/InsertionSort.java
+++ 7-Sorting/Sort/src/InsertionSort.java
@@ -1,10 +1,23 @@
+import java.util.Arrays;
+
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr= new int[]{5,4,3,2,1};
         insertion(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void insertion(int[] arr) {
-    //partial sorting nigga os damn
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j >0 ; j--) {
+                if (arr[j]<arr[j-1]){
+                    int temp =arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }else{
+                    break;
+                }
+            }
+        }
     }
 }
